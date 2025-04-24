7ceba8c053670f86f5373e04ad65c7b998af3b3a
Alice-por <alice@portraiteyewear.com>
2025-04-24 10:07:41 +0200

    Update getSlots.js

--- api/getSlots.js
+++ api/getSlots.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { brands, country, postalCode } = req.query;
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbz6UxG1aqIa1nd4gjXTYt7l0ovDVHIk9-3IlUNCc60D0FwF5sHsYbV0NmB0PNNec5oL7A/exec" +
+    const response = await fetch("https://script.google.com/s/AKfycbwX-zBEtpXT4b0XGmsHHsqM0alJHQWvw1eMFUthmSv0pjunr_7REPS4x5xmLM7e56HUsw/exec" +
       `?action=getSlots&brands=${brands}&country=${country}&postalCode=${postalCode}`);
     const data = await response.json();
     res.status(200).json(data);
